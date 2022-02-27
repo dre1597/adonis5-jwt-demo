ac41c10fe18648e1fcf9a871b79e49d971356f49
André Pereira <andrepereira97@yahoo.com>
2022-02-27 12:00:37 -0300

    :sparkles: add lucid and auth packages

--- env.ts
+++ env.ts
@@ -21,4 +21,5 @@ export default Env.rules({
   APP_NAME: Env.schema.string(),
   DRIVE_DISK: Env.schema.enum(['local'] as const),
   NODE_ENV: Env.schema.enum(['development', 'production', 'testing'] as const),
+  DB_CONNECTION: Env.schema.string(),
 })
